e4e3015600997579ec39dac46c5b3f7c4e7474a0
Alan A. Perez <alan80perez@gmail.com>
2023-03-28 14:17:46 -0400

    Trying to make migrations of different models

--- Ecommerce_Api/Api/views.py
+++ Ecommerce_Api/Api/views.py
@@ -1,8 +1,10 @@
 from django.views import View
-from django.http.response import JsonResponse
+from django.http.response import JsonResponse, HttpResponse
 from .models import Product
 # Create your views here.
+
 class ProductView(View):
+    
     def get(self, request):
         products = list(Product.objects.values())
         if(len(products) > 0):
@@ -14,6 +16,7 @@ class ProductView(View):
             data={
                 'message': 'There are no products in the database'
             }
+            return HttpResponse(status=404)
         return JsonResponse(data)
 
     def post(self, request):
